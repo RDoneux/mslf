51e6d4c31619579e7677b61f5758144f45bd3f4c
Rob Doneux <robertdoneux@gmail.com>
2024-11-22 11:37:21 +0000

    provides profile configuration from .env file

--- src/main/java/com/rdoneux/mslf/controllers/PoemController.java
+++ src/main/java/com/rdoneux/mslf/controllers/PoemController.java
@@ -3,12 +3,14 @@ package com.rdoneux.mslf.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rdoneux.mslf.models.Poem;
 import com.rdoneux.mslf.services.PoemService;
 
 @RestController
+@RequestMapping("/poems")
 public class PoemController {
 
     private PoemService poemService;
