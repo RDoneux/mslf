e53030b12e86491353d6ff088e7314234cd9ce73
Rob Doneux <robertdoneux@gmail.com>
2024-11-22 11:37:21 +0000

    provides profile configuration from .env file

---
+++ src/main/resources/db/migration/V1_0__POPULATE_DEFAULT_USER_INFO.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+    user (
+        id,
+        first_name,
+        last_name,
+        tag,
+        about
+    )
+VALUES (
+        'f6961247-4508-490f-8a1d-18f41d2cf8c8',
+        'John',
+        'Snow',
+        'Poet & Blogger',
+        'Qui fugiat culpa dolor eiusmod. Dolore nisi sit enim fugiat cillum sit qui elit laboris magna labore in irure proident. Lorem cillum Lorem fugiat pariatur nulla. Dolor ex aliqua laboris enim Lorem nulla ut irure sint ex labore cupidatat adipisicing nostrud.
+        Incididunt incididunt non ea sunt nostrud incididunt pariatur nostrud ex officia. Incididunt sint excepteur magna deserunt nisi qui aliqua irure eu amet non ex occaecat. In aute amet dolor excepteur elit incididunt exercitation aute laboris ut ut qui. Non adipisicing mollit esse nulla nostrud ex nostrud cillum laborum. Eiusmod esse tempor occaecat in in. Elit ut amet adipisicing eu duis velit nostrud. Tempor in adipisicing elit amet cupidatat ea adipisicing mollit mollit culpa exercitation dolor cillum aliqua.
+        Mollit laborum commodo nostrud non sint mollit. Anim excepteur aliquip amet ea occaecat nisi veniam. Aute id sunt minim aliquip laborum occaecat.'
+    )
--- src/test/java/com/rdoneux/mslf/MslfApplicationTests.java
+++ src/test/java/com/rdoneux/mslf/MslfApplicationTests.java
@@ -1,13 +1,8 @@
 package com.rdoneux.mslf;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MslfApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
