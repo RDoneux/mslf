11a5e709ff1b59419eda43810820db0eb5b74bf1
Rob Doneux <robertdoneux@gmail.com>
2024-11-27 16:46:48 +0000

    updates blogs schema

--- src/main/resources/db/migration/V0_2__CREATE_BLOG_TABLE.sql
+++ src/main/resources/db/migration/V0_2__CREATE_BLOG_TABLE.sql
@@ -1,5 +1,5 @@
 CREATE TABLE blogs (
-    id VARCHAR(36) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
     content TEXT,
     author VARCHAR(255) NOT NULL,
